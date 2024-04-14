1a1f04cdde1e9d8376621b18331e0f7bbbaa721b
Yash-ak-3322 <yaakbari1@myseneca.ca>
2024-04-14 19:28:11 -0400

    Update page.js

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -128,7 +128,7 @@ export default function AdminAddNewProduct() {
     if (res.success) {
       setComponentLevelLoader({ loading: false, id: "" });
       toast.success(res.message, {
-        position: toast.POSITION.TOP_RIGHT,
+        position: "top-right",
       });
 
       setFormData(initialFormData);
@@ -138,7 +138,7 @@ export default function AdminAddNewProduct() {
       }, 1000);
     } else {
       toast.error(res.message, {
-        position: toast.POSITION.TOP_RIGHT,
+        position: "top-right",
       });
       setComponentLevelLoader({ loading: false, id: "" });
       setFormData(initialFormData);
