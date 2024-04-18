d913bdd709eb81bd2bc17d93a7bf11f9145259c2
Yash-ak-3322 <158524775+Yash-ak-3322@users.noreply.github.com>
2024-04-18 19:53:44 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -25,15 +25,15 @@ export default function Home() {
 
   console.log(products);
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+ return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-12 lg:p-24">
       <section className="">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
-              Best Fashion Collection
+            <h1 className="max-w-2xl mb-4 text-3xl font-bold tracking-tight leading-tight md:text-4xl lg:text-5xl text-blue-900">
+              Top Branded Fashion Showroom
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+            <p className="max-w-2xl mb-6 text-sm font-light text-gray-700 lg:mb-8 md:text-lg lg:text-xl">
               The largest E-Commerce shopping platform of Canada :-)
               E-Commercery ☺️
             </p>
@@ -41,36 +41,37 @@ export default function Home() {
             <button
               type="button"
               onClick={() => router.push("/product/listing/all-products")}
-              className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+              className="inline-block px-4 py-2 text-xs font-medium tracking-wide uppercase bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
             >
               Explore Shop Collection
             </button>
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <div className="hidden lg:block lg:col-span-5">
             <img
-              src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              src="https://wp.blahface.com/wp-content/uploads/2021/03/BF_shopping.jpg"
               alt="Explore Shop Collection"
+              className="w-full rounded-lg"
             />
           </div>
         </div>
         <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
-            <div className="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
+            <div className="grid p-4 bg-blue-100 rounded-lg place-content-center">
               <div className="max-w-md mx-auto text-center lg:text-left">
                 <div>
-                  <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-                    Summer Sale Collection
+                  <h2 className="text-xl font-bold text-gray-900 sm:text-2xl">
+                    Brand New Season Collection
                   </h2>
                 </div>
                 <button
                   onClick={() => router.push("/product/listing/all-products")}
-                  className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  className="mt-2 inline-block px-4 py-2 text-xs font-medium tracking-wide uppercase bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                 >
                   Shop ALL
                 </button>
               </div>
             </div>
-            <div className="lg:col-span-2 lg:py-8">
+            <div className="lg:col-span-2 lg:py-4">
               <ul className="grid grid-cols-2 gap-4">
                 {products && products.length
                   ? products
@@ -109,7 +110,7 @@ export default function Home() {
         </div>
         <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
           <div className="text-center">
-            <h2 className="text-xl font-bold text-gray-950 sm:text-3xl">
+            <h2 className="text-lg font-bold text-gray-950 sm:text-2xl">
               SHOP BY CATEGORY
             </h2>
           </div>
@@ -117,14 +118,15 @@ export default function Home() {
             <li>
               <div className="relative block group">
                 <img
-                  src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                  className="object-cover w-full aspect-square"
+                  src="https://thumbs.dreamstime.com/b/kids-shopping-smiling-little-girl-bags-gifts-happy-child-holds-lot-sale-holidays-concept-105009088.jpg"
+                  className="object-cover w-auto  aspect-square"
+                  alt="Kids Category"
                 />
-                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-4 bg-black bg-opacity-50">
                   <h3 className="text-xl font-medium text-white">KIDS</h3>
                   <button
                     onClick={() => router.push("/product/listing/kids")}
-                    className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    className="mt-2 inline-block px-4 py-2 text-xs font-medium tracking-wide uppercase bg-white text-black rounded-md hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
                   >
                     Shop Now
                   </button>
@@ -134,31 +136,33 @@ export default function Home() {
             <li>
               <div className="relative block group">
                 <img
-                  src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                  className="object-cover w-full aspect-square"
+                  src="https://wallpapercave.com/wp/wp6488829.jpg"
+                  className="object-cover w-auto rounded aspect-square"
+                  alt="Women Category"
                 />
-                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-4 bg-black bg-opacity-50">
                   <h3 className="text-xl font-medium text-white">WOMEN</h3>
                   <button
                     onClick={() => router.push("/product/listing/women")}
-                    className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    className="mt-2 inline-block px-4 py-2 text-xs font-medium tracking-wide uppercase bg-white text-black rounded-md hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
                   >
                     Shop Now
                   </button>
                 </div>
               </div>
             </li>
-            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+            <li className="lg:col-span-2 lg:col-start-2">
               <div className="relative block group">
                 <img
-                  src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
-                  className="object-cover w-full aspect-square"
+                  src="https://thumbs.dreamstime.com/b/positive-man-enjoying-shopping-happy-man-shopping-bags-isolated-white-excited-happy-man-doing-online-shopping-thanks-150853335.jpg"
+                  className="object-cover w-auto rounded aspect-square"
+                  alt="Men Category"
                 />
-                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-4 bg-black bg-opacity-50">
                   <h3 className="text-xl font-medium text-white">MEN</h3>
                   <button
                     onClick={() => router.push("/product/listing/men")}
-                    className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    className="mt-2 inline-block px-4 py-2 text-xs font-medium tracking-wide uppercase bg-white text-black rounded-md hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
                   >
                     Shop Now
                   </button>
